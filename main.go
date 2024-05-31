a436dd3b3e9246c1318c2f7c4afcd8e6136e2b70
raj <brk8883@gmail.com>
2024-05-30 20:04:49 -0400

    Added CelsiusToFahrenheit function

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+// CelsiusToFahrenheit converts a temperature from Celsius to Fahrenheit.
+func CelsiusToFahrenheit(c float64) float64 {
+	return c*9/5 + 32
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
