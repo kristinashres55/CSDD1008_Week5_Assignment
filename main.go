f30db3fae1a51586a911b970c16449e6dde8e27d
Lilamoth <ahmadraja@loyalistcollege.com>
2024-05-30 20:38:15 -0400

    Added FtoC Function

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 // CelsiusToFahrenheit converts a temperature from Celsius to Fahrenheit.
 func CelsiusToFahrenheit(c float64) float64 {
 	return c*9/5 + 32
+
+}
+
+// FahrenheitToCelsius converts a temperature from Fahrenheit to Celsius.
+func FahrenheitToCelsius(f float64) float64 {
+	return (f - 32) * 5 / 9
 }
 
 func main() {
